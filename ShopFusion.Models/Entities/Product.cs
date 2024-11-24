1f1ba387cb3f8ef3dae9e0676e97612f52839cb6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-11-24 17:32:01 +0500

    One way data binding implemented.

---
+++ ShopFusion.Models/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopFusion.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
