6369b952c104bbac139538378155d15e04816c07
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-11-24 19:13:21 +0500

    Dropdown binding implemented.

--- ShopFusion.Models/Entities/Product.cs
+++ ShopFusion.Models/Entities/Product.cs
@@ -6,5 +6,13 @@
         public string Name { get; set; }
         public bool IsActive { get; set; }
         public double Price { get; set; }
+        public IEnumerable<ProductProperties> Attributes { get; set; }
+    }
+
+    public class ProductProperties
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
     }
 }
