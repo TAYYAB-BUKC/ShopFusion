3b6d1aa0d4c62c38beabe381d3566e396d853ac2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-02 02:16:36 +0500

    JSRuntimeExtension implemented.

---
+++ ShopFusion.Server/HelperClasses/JSRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace ShopFusion.Server.HelperClasses
+{
+    public static class JSRuntimeExtension
+    {
+        public static async ValueTask ShowSuccessToastrNotification(this IJSRuntime _JSRuntime, string message) 
+        {
+            await _JSRuntime.InvokeVoidAsync("ShowToastrNotification", "success", message);
+        }
+
+        public static async ValueTask ShowFailureToastrNotification(this IJSRuntime _JSRuntime, string message)
+        {
+            await _JSRuntime.InvokeVoidAsync("ShowToastrNotification", "error", message);
+        }
+    }
+}
