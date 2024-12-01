a027be11398881a9f26188affb27fa49da131410
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-02 01:42:11 +0500

    Toastr JS implemented.

---
+++ ShopFusion.Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastrNotification = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, 'Success', { timeOut: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, 'Error', { timeOut: 5000 });
+    }
+}
