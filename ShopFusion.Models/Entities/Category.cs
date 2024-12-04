e1d1a3b303b9fa49a93a435e780136f56b1a3941
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-04 05:25:33 +0500

    Setup ApplicationDbContext.

---
+++ ShopFusion.Models/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopFusion.Models.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
