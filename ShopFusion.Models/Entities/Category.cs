8a3e12e1e676057a503077a25e55a740b5d3f302
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-05 05:41:29 +0500

    Migrations implemented for category table.

--- ShopFusion.Models/Entities/Category.cs
+++ ShopFusion.Models/Entities/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace ShopFusion.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopFusion.Models.Entities
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
