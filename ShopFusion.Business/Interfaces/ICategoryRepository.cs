36c3ece335176cba15505ed22e520adc80fc0ff0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-05 05:49:54 +0500

    Interface implemented for Category.

---
+++ ShopFusion.Business/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Business.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        public List<CategoryDTO> GetAll();
+        public CategoryDTO GetById(int id);
+        public CategoryDTO Create(CategoryDTO category);
+        public CategoryDTO Update(CategoryDTO category);
+        public CategoryDTO Delete(int id);
+    }
+}
