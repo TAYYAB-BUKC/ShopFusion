89540e049c1b375014742e4bb24cca537282c0ac
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-07 21:22:11 +0500

    Validations implemented.

--- ShopFusion.Models/DTOs/CategoryDTO.cs
+++ ShopFusion.Models/DTOs/CategoryDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace ShopFusion.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopFusion.Models.DTOs
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name")]
         public string Name { get; set; }
     }
 }
