f0e3ea95ed425bbe526747fa924dac1dd4c1d6df
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-08 04:05:41 +0500

    Delete confirmation implemented on manage category screen.

--- ShopFusion.Server/wwwroot/js/common.js
+++ ShopFusion.Server/wwwroot/js/common.js
@@ -10,3 +10,11 @@
 window.ShowSweetAlertNotification = (icon, title, message) => {
     swal.fire({ icon: icon, title: title, text: message});
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
