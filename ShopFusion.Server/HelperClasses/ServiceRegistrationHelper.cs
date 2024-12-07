528c7377615a031f8cde8e0289c10891a53ea8f7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-07 22:59:56 +0500

    Add functionality implemented for manage category screen.

--- ShopFusion.Server/HelperClasses/ServiceRegistrationHelper.cs
+++ ShopFusion.Server/HelperClasses/ServiceRegistrationHelper.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using ShopFusion.Business.Interfaces;
 using ShopFusion.Business.Repositories;
 using ShopFusion.DataAccess.Data;
+using ShopFusion.Models.Mappers;
 using ShopFusion.Server.Data;
 
 namespace ShopFusion.Server.HelperClasses
@@ -16,7 +18,7 @@ namespace ShopFusion.Server.HelperClasses
 			builder.Services.AddSingleton<WeatherForecastService>();
 			builder.Services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+			builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 			builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 		}
