c510b13549cc39c1e466ff95cdea4e56bcdc1fdc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-09 02:27:19 +0500

    Product entity updated.

--- ShopFusion.Models/Entities/Product.cs
+++ ShopFusion.Models/Entities/Product.cs
@@ -1,13 +1,31 @@
-﻿namespace ShopFusion.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopFusion.Models.Entities
 {
-    public class Product
+	//public class Product
+	//{
+	//	public int Id { get; set; }
+	//	public string Name { get; set; }
+	//	public bool IsActive { get; set; }
+	//	public double Price { get; set; }
+	//	public IEnumerable<ProductProperties> Attributes { get; set; }
+	//}
+
+	public class Product
     {
+		[Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool IsActive { get; set; }
-        public double Price { get; set; }
-        public IEnumerable<ProductProperties> Attributes { get; set; }
-    }
+		public string Description { get; set; }
+		public bool IsShopFavorite{ get; set; }
+		public bool IsCustomerFavorite { get; set; }
+        public string Color { get; set; }
+		public string ImageURL { get; set; }
+		public int CategoryId { get; set; }
+		[ForeignKey("CategoryId")]
+		public Category Category { get; set; }
+	}
 
     public class ProductProperties
     {
