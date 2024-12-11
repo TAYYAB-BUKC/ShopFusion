20388cea2d190e03c8dc1121932aa6b5214c7c6b
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-11 05:47:32 +0500

    Product Prices Entity Introduced.

---
+++ ShopFusion.Models/Entities/ProductPrices.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopFusion.Models.Entities
+{
+	public class ProductPrices
+	{
+		public int Id { get; set; }
+		public int ProductId { get; set; }
+		[ForeignKey(nameof(ProductId))]
+		public Product Product { get; set; }
+		public string Size { get; set; }
+		public double Price { get; set; }
+	}
+}
