cb2453634b0c17cf9e7cfb1710265c014b4548b1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-11 06:04:02 +0500

    Product Prices DTO introduced.

---
+++ ShopFusion.Models/DTOs/ProductPricesDTO.cs
@@ -0,0 +1,18 @@
+﻿using ShopFusion.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopFusion.Models.DTOs
+{
+	public class ProductPricesDTO
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Please select the product")]
+		public int ProductId { get; set; }
+		[Required(ErrorMessage = "Please enter the size")]
+		public string Size { get; set; }
+		[Required]
+		[Range(1, Int64.MaxValue, ErrorMessage = "Please enter the correct price")]
+		public double Price { get; set; }
+	}
+}
