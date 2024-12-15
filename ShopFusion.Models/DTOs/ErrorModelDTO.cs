aa4319c1c4a73db02e6be8ab827c68e93580784c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-15 07:53:32 +0500

    Success and Error DTOs added for API.

---
+++ ShopFusion.Models/DTOs/ErrorModelDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopFusion.Models.DTOs
+{
+	public class ErrorModelDTO
+	{
+		public string Title { get; set; }
+		public int StatusCode { get; set; }
+		public string Message { get; set; }
+	}
+}
---
+++ ShopFusion.Models/DTOs/SuccessModelDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopFusion.Models.DTOs
+{
+	public class SuccessModelDTO
+	{
+		public string Title { get; set; }
+		public int StatusCode { get; set; }
+		public string Message { get; set; }
+		public object Data { get; set; }
+	}
+}
