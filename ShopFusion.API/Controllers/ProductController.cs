a80f6870e33cee7fb23cde4e32a270008448abc6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-15 08:15:02 +0500

    GetById endpoint implemented for Product in API.

--- ShopFusion.API/Controllers/ProductController.cs
+++ ShopFusion.API/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ShopFusion.Business.Interfaces;
+using ShopFusion.Models.DTOs;
 
 namespace ShopFusion.API.Controllers
 {
@@ -18,6 +19,31 @@ namespace ShopFusion.API.Controllers
 		public async Task<IActionResult> GetAll() 
 		{
 			return Ok(await _productRepository.GetAll());
+		}
+
+		[HttpGet("{productId}")]
+		public async Task<IActionResult> GetById(int? productId)
+		{
+			if(productId == null || productId == 0)
+			{
+				return BadRequest(new ErrorModelDTO()
+				{
+					Message = "Please specify product id",
+					StatusCode = StatusCodes.Status400BadRequest
+				});
+			}
+
+			var product = await _productRepository.GetById(productId.Value);
+			if(product.Id <= 0)
+			{
+				return BadRequest(new ErrorModelDTO()
+				{
+					Message = "Product not found",
+					StatusCode = StatusCodes.Status404NotFound
+				});
+			}
+
+			return Ok(product);
 		}
 	}
 }
