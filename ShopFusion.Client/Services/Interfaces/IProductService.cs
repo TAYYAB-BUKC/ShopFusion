2340daa4b8f200b8e375b4231711c06b2def53bd
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-15 21:23:50 +0500

    Interface implemented for fetching products.

---
+++ ShopFusion.Client/Services/Interfaces/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Services.Interfaces
+{
+	public interface IProductService
+	{
+		public Task<IEnumerable<ProductDTO>> GetAllProducts();
+		public Task<ProductDTO> GetProductById(int productId);
+	}
+}
