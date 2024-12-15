52d062b354fe055246bb38e650191e33c3a849eb
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-15 18:53:07 +0500

    Toastr and Modals functions implemented.

---
+++ ShopFusion.Client/wwwroot/js/common.js
@@ -0,0 +1,20 @@
+﻿window.ShowToastrNotification = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, 'Success', { timeOut: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, 'Error', { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlertNotification = (icon, title, message) => {
+    swal.fire({ icon: icon, title: title, text: message});
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
