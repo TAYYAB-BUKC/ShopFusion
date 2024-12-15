3821f8f839fb371cd1cd397530dc6a088e1afc44
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-15 07:47:41 +0500

    DbContext and AutoMapper added in API.

---
+++ ShopFusion.API/HelperClasses/ServiceRegistrationHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopFusion.DataAccess.Data;
+using ShopFusion.Models.Mappers;
+
+namespace ShopFusion.API.HelperClasses
+{
+	public class ServiceRegistrationHelper
+	{
+		public static void RegisterServices(WebApplicationBuilder builder)
+		{
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+			builder.Services.AddAutoMapper(typeof(MappingProfile));
+		}
+	}
+}
--- ShopFusion.API/Program.cs
+++ ShopFusion.API/Program.cs
@@ -1,11 +1,7 @@
+using ShopFusion.API.HelperClasses;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ServiceRegistrationHelper.RegisterServices(builder);
 
 var app = builder.Build();
 
@@ -17,6 +13,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
