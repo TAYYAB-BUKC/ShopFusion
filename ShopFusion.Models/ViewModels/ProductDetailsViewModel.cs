988675014c8aa658344b70f5a44a8cedefe9c014
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-18 07:22:37 +0500

    ProductDetailsViewModel added for cart functionality.

---
+++ ShopFusion.Models/ViewModels/ProductDetailsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ShopFusion.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopFusion.Models.ViewModels
+{
+	public class ProductDetailsViewModel
+	{
+		[Range(1, Int64.MaxValue, ErrorMessage = "Please enter a value greater than 0.")]
+		public int Count { get; set; }
+		[Required]
+		public int SelectedProductPriceID { get; set; }
+		public ProductPricesDTO	ProductPrice { get; set; }
+
+		public ProductDetailsViewModel()
+		{
+			Count = 1;
+			SelectedProductPriceID = 0;
+			ProductPrice = new ProductPricesDTO();
+		}
+	}
+}
