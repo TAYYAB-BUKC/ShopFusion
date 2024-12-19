6bc5407607fe8ebd6993f1b78a7263e4934b2961
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-19 10:09:17 +0500

    ViewModel added for cart.

---
+++ ShopFusion.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Models.ViewModels
+{
+	public class CartViewModel
+	{
+		public int ProductId { get; set; }
+		public ProductDTO Product { get; set; }
+		public int SelectedProductPriceId { get; set; }
+		public ProductPricesDTO ProductPrice { get; set; }
+		public int Count { get; set; }
+	}
+}
