91a21307e1a9d16074017256ccb97f0f3df5ae8a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-20 18:27:36 +0500

    Models added for Order.

---
+++ ShopFusion.Models/Entities/Order.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopFusion.Models.Entities
+{
+	public class Order
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public int UserId { get; set; }
+		[Required]
+		[Display(Name = "Grand Total")]
+		public double GrandTotal { get; set; }
+		[Required]
+		[Display(Name = "Order Date")]
+		public DateTime OrderDate { get; set; }
+		[Required]
+		[Display(Name = "Shipping Date")]
+		public DateTime ShippingDate { get; set; }
+		[Required] 
+		public string Status { get; set; }
+		
+		public string? SessionId { get; set; }
+		public string? PaymentIntentId { get; set; }
+
+		[Required]
+		public string Name { get; set; }		
+		[Required]
+		[Display(Name = "Phone Number")]
+		public string? PhoneNumber { get; set; }
+		[Required]
+		[Display(Name = "Street Address")]
+		public string? StreetAddress { get; set; }
+		[Required]
+		public string State { get; set; }
+		[Required]
+		public string City { get; set; }
+		[Required]
+		[Display(Name = "Postal Code")]
+		public string PostalCode { get; set; }
+
+	}
+}
---
+++ ShopFusion.Models/Entities/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopFusion.Models.Entities
+{
+	public class OrderDetails
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public int OrderId { get; set; }
+		public int ProductId { get; set; }
+		[ForeignKey("ProductId")]
+		[NotMapped]
+		public virtual Product Product { get; set; }
+		[Required] 
+		public int Count { get; set; }
+		[Required] 
+		public double Price { get; set; }
+		[Required] 
+		public string Size { get; set; }
+		[Required] 
+		public string ProductName { get; set; }
+	}
+}
