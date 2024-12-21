a25512aa16008dab65534e90b175b618c61be7ec
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-21 11:28:30 +0500

    Order service implemented in Client.

---
+++ ShopFusion.Client/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Services.Interfaces
+{
+	public interface IOrderService
+	{
+		public Task<IEnumerable<CustomOrderDTO>> GetAllOrders();
+		public Task<CustomOrderDTO> GetOrderById(int orderId);
+	}
+}
---
+++ ShopFusion.Client/Services/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using ShopFusion.Client.Services.Interfaces;
+using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly HttpClient _httpClient;
+		private readonly string _apiBaseURL;
+
+		public OrderService(HttpClient httpClient, IConfiguration configuration)
+		{
+			_httpClient = httpClient;
+			_apiBaseURL = configuration.GetSection("API_BASEURL").Value;
+		}
+
+		public async Task<IEnumerable<CustomOrderDTO>> GetAllOrders()
+		{
+			var orders = new List<CustomOrderDTO>();
+			var response = await _httpClient.GetAsync($"{_apiBaseURL}/Order/GetAll");
+			if (response.IsSuccessStatusCode)
+			{
+				var content = await response.Content.ReadAsStringAsync();
+				orders = JsonConvert.DeserializeObject<List<CustomOrderDTO>>(content);
+			}
+			return orders;
+		}
+
+		public async Task<CustomOrderDTO> GetOrderById(int orderId)
+		{
+			var order = new CustomOrderDTO();
+			var response = await _httpClient.GetAsync($"{_apiBaseURL}/Order/GetById/{orderId}");
+			if (response.IsSuccessStatusCode)
+			{
+				var content = await response.Content.ReadAsStringAsync();
+				order = JsonConvert.DeserializeObject<CustomOrderDTO>(content);
+			}
+
+			return order;
+		}
+	}
+}
