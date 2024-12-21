26316c5ca6690d273fa7a2b1713ec49c1be3653f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-21 11:11:32 +0500

    GetAll and GetById endpoints implemented in OrderController in API

---
+++ ShopFusion.API/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopFusion.Business.Interfaces;
+using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.API.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class OrderController : ControllerBase
+	{
+		public IOrderRepository _orderRepository { get; set; }
+		public OrderController(IOrderRepository orderRepository)
+		{
+			_orderRepository = orderRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			return Ok(await _orderRepository.GetAll(null, null));
+		}
+
+		[HttpGet("{orderId}")]
+		public async Task<IActionResult> GetById(int? orderId)
+		{
+			if (orderId == null || orderId == 0)
+			{
+				return BadRequest(new ErrorModelDTO()
+				{
+					Message = "Please specify order id",
+					StatusCode = StatusCodes.Status400BadRequest
+				});
+			}
+
+			var order = await _orderRepository.GetById(orderId.Value);
+			if (order.Order.Id <= 0)
+			{
+				return BadRequest(new ErrorModelDTO()
+				{
+					Message = "Order not found",
+					StatusCode = StatusCodes.Status404NotFound
+				});
+			}
+
+			return Ok(order);
+		}
+	}
+}
