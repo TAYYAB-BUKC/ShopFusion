c4a3a483188572412960ea8c512e920fabe838f8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-21 10:02:38 +0500

    Interface added for Order Repository.

---
+++ ShopFusion.Business/Interfaces/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Business.Interfaces
+{
+	public interface IOrderRepository
+	{
+		public Task<List<CustomOrderDTO>> GetAll(string? userId, string? status);
+		public Task<CustomOrderDTO> GetById(int orderId);
+		public Task<CustomOrderDTO> Create(CustomOrderDTO order);
+		public Task<int> DeleteById(int orderId);
+		public Task<List<OrderDTO>> Update(OrderDTO order);
+		public Task<List<OrderDTO>> MarkPaymentSuccessful(int id);
+		public Task<List<bool>> UpdateStatus(int orderId, string status);
+	}
+}
