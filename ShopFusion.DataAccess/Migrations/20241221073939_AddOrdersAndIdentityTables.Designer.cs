2cfd150e59dc148fd1fc07f9ae8afbf17f7d31a0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-21 12:44:10 +0500

    Migration added for Orders And Identity tables.

--- ShopFusion.DataAccess/Migrations/20241221073939_AddOrdersAndIdentityTables.Designer.cs
+++ ShopFusion.DataAccess/Migrations/20241221073939_AddOrdersAndIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopFusion.DataAccess.Data;
 
@@ -11,9 +12,10 @@ using ShopFusion.DataAccess.Data;
 namespace ShopFusion.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241221073939_AddOrdersAndIdentityTables")]
+    partial class AddOrdersAndIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
