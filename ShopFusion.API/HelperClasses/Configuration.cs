ec605a4ece5e51c3f45f3d61bddfa595aab8e88d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-22 15:55:03 +0500

    More configuration added in appSettings for authentication token.

---
+++ ShopFusion.API/HelperClasses/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopFusion.API.HelperClasses
+{
+	public class Configuration
+	{
+		public string SecretKey { get; set; }
+		public string ValidAudience { get; set; }
+		public string ValidIssuer { get; set; }
+	}
+}
