d881597c9637addda34119791f667a9f5b25d12a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-22 12:30:50 +0500

    1) DbInitializer service implemented. 2) Apply pending migrations automatically.

---
+++ ShopFusion.Server/Services/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ShopFusion.Server.Services.Interfaces;
+using ShopFusion.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopFusion.Server.Services
+{
+	public class DbInitializer : IDbInitializer
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+		private readonly ApplicationDbContext _dbContext;
+
+		public DbInitializer(UserManager<IdentityUser> userManager,
+			RoleManager<IdentityRole> roleManager,
+			ApplicationDbContext dbContext)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+			_dbContext = dbContext;
+		}
+
+		public void Initialize()
+		{
+			if(_dbContext.Database.GetPendingMigrationsAsync().GetAwaiter().GetResult().Count() > 0)
+			{
+				_dbContext.Database.Migrate();
+			}
+		}
+	}
+}
---
+++ ShopFusion.Server/Services/Interfaces/IDbInitializer.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace ShopFusion.Server.Services.Interfaces
+{
+	public interface IDbInitializer
+	{
+		void Initialize();
+	}
+}
