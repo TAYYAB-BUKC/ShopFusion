1bd643745498c8b7e10fa93c5fd015e370339c17
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-23 23:46:34 +0500

    AuthenticationService interface added.

---
+++ ShopFusion.Client/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Services.Interfaces
+{
+	public interface IAuthenticationService
+	{
+		Task<SignUpResponseDTO> Register(SignUpRequestDTO signUpRequestDTO);
+		Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+		Task Logout();
+	}
+}
