4323eb9ce9f067397d518aed425338239078ae2a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-24 20:08:57 +0500

    Move register code to separate file.

---
+++ ShopFusion.Client/Pages/Register.razor.cs
@@ -0,0 +1,26 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Pages
+{
+	public partial class Register
+	{
+		public SignUpRequestDTO signUpRequestDTO { get; set; } = new();
+		public bool ShowRegistrationErrors { get; set; } = false;
+		public IEnumerable<string> Errors { get; set; }
+
+		private async Task RegisterUser()
+		{
+			ShowRegistrationErrors = false;
+			var response = await _authenticationService.Register(signUpRequestDTO);
+			if (response.IsSuccessful)
+			{
+				_navigationManager.NavigateTo("/login");
+			}
+			else
+			{
+				ShowRegistrationErrors = true;
+				Errors = response.Errors;
+			}
+		}
+	}
+}
