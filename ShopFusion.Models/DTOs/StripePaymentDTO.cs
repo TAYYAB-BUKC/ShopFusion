188571676ce0b93d780c29f4c0d2534146feaaaa
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-25 17:53:15 +0500

    Success and Cancel URLs aded in StripePaymentDTO.

--- ShopFusion.Models/DTOs/StripePaymentDTO.cs
+++ ShopFusion.Models/DTOs/StripePaymentDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace ShopFusion.Models.DTOs
 {
 	public class StripePaymentDTO
 	{
+		public StripePaymentDTO()
+		{
+			SuccessURL = "OrderConfirmation";
+			CancelURL = "Checkout";
+		}
+
 		public CustomOrderDTO Order { get; set; }
+		public string SuccessURL { get; set; }
+		public string CancelURL { get; set; }
 	}
 }
