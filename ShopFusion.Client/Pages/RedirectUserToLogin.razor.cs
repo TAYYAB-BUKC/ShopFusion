f76256ccf7b6ef74c2cf91ea130006b0b06acd6a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-25 12:06:11 +0500

    User redirection to login implemented if user is not logged in.

---
+++ ShopFusion.Client/Pages/RedirectUserToLogin.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace ShopFusion.Client.Pages
+{
+	public partial class RedirectUserToLogin
+	{
+		[CascadingParameter]
+		public Task<AuthenticationState> AuthenticationState { get; set; }
+		[Inject]
+		public NavigationManager NavigationManager { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			var authenticationState = await AuthenticationState;
+
+			if (authenticationState.User.Identity is null || !authenticationState.User.Identity.IsAuthenticated)
+			{
+				NavigationManager.NavigateTo("/login");
+			}
+		}
+	}
+}
