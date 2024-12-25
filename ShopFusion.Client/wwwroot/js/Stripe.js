464a9f117dde7b94d1bf773fe3b9e1d497e3ac9a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-25 19:01:51 +0500

    Stripe JS added in Client.

---
+++ ShopFusion.Client/wwwroot/js/Stripe.js
@@ -0,0 +1,4 @@
+﻿RedirectUserToStripePayment = function (id) {
+	var stripe = Stripe("pk_test_51QZtMhDyP50ifadOEJZ9iBhaJl27cZEnCWbToJdg6jqPl4eCYXEXnzZgnvfTtPspeiQtQ5m0zoNgNK7fHdCUhZBT00Oidkbt7v");
+	stripe.redirectToCheckout({ sessionId: id });
+}
