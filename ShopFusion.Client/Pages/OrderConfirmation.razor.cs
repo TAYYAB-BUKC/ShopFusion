8c494cff4604f8ea7e5a845d75503b2594b8fe88
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-26 23:07:08 +0500

    Code of order confirmation moved to separate file.

---
+++ ShopFusion.Client/Pages/OrderConfirmation.razor.cs
@@ -0,0 +1,10 @@
+﻿using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Pages
+{
+	public partial class OrderConfirmation
+	{
+		private bool IsProcessing { get; set; } = false;
+		private CustomOrderDTO Order { get; set; }
+	}
+}
