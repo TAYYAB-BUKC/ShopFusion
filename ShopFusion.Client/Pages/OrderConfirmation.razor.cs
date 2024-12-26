acfdd29e7952426024bfb77079da3d3b0c95f0c1
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-26 23:10:29 +0500

    Populate order from local storage.

--- ShopFusion.Client/Pages/OrderConfirmation.razor.cs
+++ ShopFusion.Client/Pages/OrderConfirmation.razor.cs
@@ -1,10 +1,23 @@
-﻿using ShopFusion.Models.DTOs;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using ShopFusion.Common;
+using ShopFusion.Models.DTOs;
+using System;
 
 namespace ShopFusion.Client.Pages
 {
 	public partial class OrderConfirmation
 	{
+		[Inject]
+		public ILocalStorageService LocalStorageService { get; set; }
 		private bool IsProcessing { get; set; } = false;
 		private CustomOrderDTO Order { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			IsProcessing = true;
+			Order = await LocalStorageService.GetItemAsync<CustomOrderDTO>(CommonConfiguration.OrderDetails_Key);
+			IsProcessing = false;
+		}
 	}
 }
