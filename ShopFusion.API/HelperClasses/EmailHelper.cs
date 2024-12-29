78e1a50a74e294fe51b17a72afe4dc35aff44314
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-29 16:18:16 +0500

    EmailHelper implemented using MimeKit and MailKit in API.

---
+++ ShopFusion.API/HelperClasses/EmailHelper.cs
@@ -0,0 +1,25 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
+namespace ShopFusion.API.HelperClasses
+{
+	public class EmailHelper : IEmailSender
+	{
+		public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+		{
+			var message = new MimeMessage();
+			message.From.Add(MailboxAddress.Parse("donotreply@test.com"));
+			message.To.Add(MailboxAddress.Parse(email));
+			message.Subject = subject;
+			message.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage };
+
+			using (var smtpClient = new SmtpClient())
+			{
+				smtpClient.Connect("smtp.freesmtpservers.com", 25, MailKit.Security.SecureSocketOptions.StartTls);
+				await smtpClient.SendAsync(message);
+				await smtpClient.DisconnectAsync(true);
+			}
+		}
+	}
+}
