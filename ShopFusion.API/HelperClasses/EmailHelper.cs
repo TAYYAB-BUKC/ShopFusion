686ce549503cc1319b9f8d930bf13c947b6d4de5
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-30 12:23:01 +0500

    Changed SecureSocketOptions to None because of free SMTP server.

--- ShopFusion.API/HelperClasses/EmailHelper.cs
+++ ShopFusion.API/HelperClasses/EmailHelper.cs
@@ -8,17 +8,24 @@ namespace ShopFusion.API.HelperClasses
 	{
 		public async Task SendEmailAsync(string email, string subject, string htmlMessage)
 		{
-			var message = new MimeMessage();
-			message.From.Add(MailboxAddress.Parse("donotreply@test.com"));
-			message.To.Add(MailboxAddress.Parse(email));
-			message.Subject = subject;
-			message.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text = htmlMessage };
-
-			using (var smtpClient = new SmtpClient())
+			try
 			{
-				smtpClient.Connect("smtp.freesmtpservers.com", 25, MailKit.Security.SecureSocketOptions.StartTls);
-				await smtpClient.SendAsync(message);
-				await smtpClient.DisconnectAsync(true);
+				var message = new MimeMessage();
+				message.From.Add(MailboxAddress.Parse("donotreply@test.com"));
+				message.To.Add(MailboxAddress.Parse(email));
+				message.Subject = subject;
+				message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+				using (var smtpClient = new SmtpClient())
+				{
+					smtpClient.Connect("smtp.freesmtpservers.com", 25, MailKit.Security.SecureSocketOptions.None);
+					await smtpClient.SendAsync(message);
+					await smtpClient.DisconnectAsync(true);
+				}
+			}
+			catch (Exception ex)
+			{
+				throw;
 			}
 		}
 	}
