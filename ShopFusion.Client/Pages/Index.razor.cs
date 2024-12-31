829fe50abf786474a899f7a04f96d22c9a22aaa4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2024-12-31 23:08:59 +0500

    Move Home code to separate file in Client.

---
+++ ShopFusion.Client/Pages/Index.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopFusion.Client.Services;
+using ShopFusion.Client.Services.Interfaces;
+using ShopFusion.Models.DTOs;
+
+namespace ShopFusion.Client.Pages
+{
+	public partial class Index
+	{
+		[Inject]
+		public IProductService ProductService { get; set; }
+
+		public bool IsProcessing { get; set; }
+		public IEnumerable<ProductDTO> products = new List<ProductDTO>();
+
+		protected override async Task OnInitializedAsync()
+		{
+			IsProcessing = true;
+			products = await ProductService.GetAllProducts();
+			IsProcessing = false;
+		}
+	}
+}
